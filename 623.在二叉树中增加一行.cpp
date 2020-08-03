d756ff5c547e60588f930023e8cd1991213eaca1
zouchengyao <32712603+zouchengyao@users.noreply.github.com>
2020-08-04 02:36:16 +0800

    Update 623.在二叉树中增加一行.cpp

--- 623.在二叉树中增加一行.cpp
+++ 623.在二叉树中增加一行.cpp
@@ -17,31 +17,67 @@
 class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int v, int d) {
-        queue<TreeNode*> q;
+        queue<TreeNode*> q, prev;
         q.push(root);
         int level = 1;
-        while(!q.empty())
+        if (d == 1) //TODO:
         {
-            auto size = q.size();
-            cout << "---------" << endl;
-            
-            while(size--)
-            {
-                auto curr = q.front();
-                cout << q.front()->val << endl;
-                q.pop();
-                
-                if (curr -> left) q.push(curr->left);
-                if (curr ->right) q.push(curr->right);
-
-            }
-
-
-            level++;
-
-            
+            auto temp = new TreeNode(v);
+            temp->left = root;
+            return temp;
         }
 
+        while(!q.empty())
+        {
+            prev = q;
+            auto size = q.size();
+            if (level == d - 1)
+            {
+                while(size--)
+                {
+                    auto curr = q.front();
+                    q.pop();
+                    auto l = curr ->left;
+                    auto r = curr ->right;
+
+                    curr -> left = new TreeNode(v);
+                    curr -> right= new TreeNode(v);
+                    if (l)
+                    {
+                        q.push(l);
+                        curr->left->left=l;
+                    }
+                    if (r)
+                    {
+                        q.push(r);
+                        curr->right->right = r;
+                    }
+                }
+            }
+
+            else
+            {
+                  while(size--)
+                {
+                    auto curr = q.front();
+                    q.pop();
+                    if (curr -> left) q.push(curr->left);
+                    if (curr ->right) q.push(curr->right);
+                }              /* code */
+            }
+            
+
+            level++;
+        }
+
+        if (level == d) 
+        while(!prev.empty())
+        {
+            auto node = prev.front();
+            node -> left = new TreeNode(v);
+            node -> right = new TreeNode(v);
+            prev.pop();
+        }
 
         return root;
     }
